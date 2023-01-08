3769fd3fbe54d242740a1cde428050520eadfdea
Youssef Mahdi <youssefmahdi.edu@gmail.com>
2023-01-08 18:26:57 +0100

    (#2) updated userContorller.js call-back function
    -delete,get,update and post routes controllers

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,3 +1,60 @@
 const EtudiantModel=require('../Models/EtudiantModel');
 const ProfessorModel=require('../Models/professorModel');
 
+module.exports.addUser=(req,res,next)=>{
+    if(req.body.type=='professor'){
+        ProfessorModel.create({
+            nom:req.body.nom,
+            email:req.body.email,
+            password:req.body.password
+        }).then((data)=>{
+            res.send(data)
+        }).catch((e)=>{
+            console.log(e);
+        })
+    }
+    else if(req.body.type=='etudiant'){
+        EtudiantModel.create({
+            nom:req.body.nom,
+            email:req.body.email,
+            password:req.body.password
+        }).then((data)=>{
+            res.send(data)
+        }).catch((e)=>{
+            console.log(e);
+        })
+    }
+    next();
+}
+module.exports.modfiyUser=(req,res,next)=>{
+    if(req.body.type=='professor'){
+        ProfessorModel.findByIdAndUpdate({_id:req.params.id},{
+            email:req.body.email,
+            password:req.body.password
+        }).then((data)=>{
+            res.send(data);
+        }).catch(e=>{
+            console.log(e);
+        })
+    }
+    else if(req.body.type=='etudiant'){
+        EtudiantModel.findByIdAndUpdate({_id:req.params.id},{
+            email:req.body.email,
+            password:req.body.password
+        }).then((data)=>{
+            res.send(data);
+        }).catch(e=>{
+            console.log(e);
+        })
+    }
+    next();
+}
+module.exports.deleteUser=(req,res,next)=>{
+    if(req.body.type=='professor'){
+        ProfessorModel.findByIdAndDelete(req.params.id).then(()=>{
+            console.log(req.params.id+" deleted")
+        }).catch((e)=>{
+            console.log(e);
+        })
+    }
+}

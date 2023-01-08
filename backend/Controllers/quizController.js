73bb9005e7713b8e56db1c511b43a2783f70ed9e
Youssef Mahdi <youssefmahdi.edu@gmail.com>
2023-01-08 17:48:45 +0100

    Update quizController.js

--- backend/Controllers/quizController.js
+++ backend/Controllers/quizController.js
@@ -21,5 +21,19 @@ module.exports.quizesByUserId=(req,res,next)=>{
 }
 
 module.exports.modifyQuiz=(req,res,next)=>{
-    
+    quizModel.findByIdAndUpdate({_id:req.params.id},req.body).then((data)=>{
+        res.send(data)
+    }).catch((e)=>{
+        console.log(e)
+    })
+    next();
+}
+
+module.exports.deleteQuiz=(req,res,next)=>{
+    quizModel.findByIdAndDelete(req.params.id).then(()=>{
+        console.log(req.params.id+" succesfully deleted");
+    }).catch((e)=>{
+        console.log(e);
+    })
+    next();
 }
